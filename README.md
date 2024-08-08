b873a7a462c46ac9befa3bbb783b2c4f7f0b2928
KevMartinsDev <oenphix@gmail.com>
2024-08-07 21:50:59 -0300

    Busca de dados e usuarios pela API do Github, Criação de objetos e tela

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# projeto-inicial-fetch-github-api
-Repositório contendo os arquivos iniciais do projeto de Fetch e GitHub API
+# GitHub User Search
+
+Este projeto utiliza a API do GitHub para buscar informações de usuários e seus repositórios. O usuário pode digitar um nome de usuário do GitHub em um campo de busca, e o projeto exibirá o nome do usuário e uma lista de seus repositórios, com links para cada um.
+
+## Funcionalidades
+
+- Busca de usuários no GitHub.
+- Exibição do nome do usuário e avatar.
+- Listagem dos repositórios do usuário com links diretos.
+
+## Tecnologias Utilizadas
+
+- **Frontend:** CSS, HTML e JavaScript
+- **API:** GitHub API
+- **Requisições HTTP:** Usando Fetch
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,13 @@
+import { getUser } from "/src/scripts/services/user.js"
+import { getRepositories } from "/src/scripts/services/repositories.js"
+
+import { user } from "/src/scripts/objects/user.js"
+import { screen } from "/src/scripts/objects/screen.js"
+
+
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
-    getUserProfile(userName)
+    getUserData(userName)
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
@@ -8,48 +15,19 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const key = e.which || e.keyCodeconst
     const isEnterKeyPressed = key === 13
     
-    if(isEnterKeyPressed){
-        getUserProfile(userName)
+    if (isEnterKeyPressed){
+        getUserData(userName)
     }
 })
 
-async function user(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}`)
-    return await response.json()
-}
+async function getUserData(userName){
 
-async function repos(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
-    return await response.json()
-}
+    const userResponse = await getUser(userName)
+    const repositoriesResponse = await getRepositories(userName)
 
-function getUserProfile(userName){
+    user.setInfo(userResponse)
+    user.setRepositories(repositoriesResponse)
 
-    user(userName).then(userData => {
-        let userInfo = `<div class="info">
-                            <img src="${userData.avatar_url}" alt="Foto do perfil  do usuário" />
-                            <div class="data">
-                                <h1>${userData.name ?? 'Não possui nome cadastrado 😢'}</h1>
-                                <p>${userData.io ?? 'Usuário não possui bio cadastrado 😢'}</p>
-                            </div>
-                        </div>`
+    screen.renderUser(user)
 
-        document.querySelector('.profile-data').innerHTML = userInfo
-
-        getUserRepositories(userName)
-    })
-}
-
-function getUserRepositories(userName){
-    repos(userName).then(reposData => {
-        let repositoriesItens = ""
-        reposData.forEach(repo => {
-            repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}></a></li>`
-        })
-
-        document.querySelector('.profile-data').innerHTML +=   `<div class="repositories section">
-                                                                    <h2>Repositórios</h2>
-                                                                    <ul>${repositoriesItens}</ul>
-                                                                </div>`
-    })
 }
